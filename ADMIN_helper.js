de4895c46dddd1ed2f312e139e6fd1c7208cef33
Frank Wu <marcfrances@outlook.com>
2020-02-27 11:43:48 +0800

    123

--- ADMIN_helper.js
+++ ADMIN_helper.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         ADMIN scripts
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       You
 // @include https://10.**
 // @include https://onion.wroclaw.nsn-rdnet.net/*
+// @include https://localhost:*
 
 
 // @grant        none
