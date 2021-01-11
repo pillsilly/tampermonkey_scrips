5d9bf8349f67c470001b3efa52b0e9786f63adee
Frank Wu <frank.1.wu@nokia-sbell.com>
2021-01-11 15:55:25 +0800

    pplhelper

--- pipeline_log_helper.js
+++ pipeline_log_helper.js
@@ -1,73 +1,78 @@
-// ==UserScript==
-// @name         pipeline log helper
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  asdfasdf
-// @author       You
-// @match        https://oam-cci.japco.scm.nsn-rdnet.net/**/?start=0
-// @grant        none
-// ==/UserScript==
+    // ==UserScript==
+    // @name         pipeline log helper
+    // @namespace    http://tampermonkey.net/
+    // @version      0.1
+    // @description  asdfasdf
+    // @author       You
+    // @match        https://oam-cci.japco.scm.nsn-rdnet.net/**/?start=0
+    // @grant        none
+    // ==/UserScript==
 
-(function () {
-    'use strict';
+    (function () {
+        'use strict';
 
-    // const fs = require('fs');
+        // const fs = require('fs');
 
-    const STEP_PREFIX = [
-        'When I',
-        'And I',
-        'Given '
-    ]
-
-    const fileContent = document.querySelector('body>pre').textContent;
-    // const fileContent = fs.readFileSync('test/1.txt', { encoding: 'utf-8' });
-    const lines = fileContent.split('\n');
-    // const lastIndex = lines.length - 1;
+        setTimeout(extract, 2000)
 
 
-    lines.forEach((line, index) => {
-        if (line.indexOf('✖ failed') < 0) return;
+
+        function extract() {
+            const STEP_PREFIX = [
+                'When I',
+                'And I',
+                'Given '
+            ]
+
+            const fileContent = document.querySelector('body>pre').textContent;
+            // const fileContent = fs.readFileSync('test/1.txt', { encoding: 'utf-8' });
+            const lines = fileContent.split('\n');
+            // const lastIndex = lines.length - 1;
+
+            lines.forEach((line, index) => {
+                //console.log(`>>>>>> ${line}`);
+                if (!line || line.indexOf('✖ failed') < 0) return;
 
 
-        const info = retriveStepAndSchenario(index)
-        console.log(info)
-    });
+                const info = retriveStepAndSchenario(index)
+                console.log(info)
+            });
+
+            function retriveStepAndSchenario(currentIndex) {
+                const toCheckLineIndex = currentIndex - 1;
+                const step = findStep(toCheckLineIndex);
+                const failureFirstLine = lines[currentIndex + 1]
+                const scenario = findScenario(toCheckLineIndex - 1)
+
+                return {
+                    failureFirstLine,
+                    index: toCheckLineIndex,
+                    step,
+                    scenario
+                }
+            }
+
+            function findScenario(index) {
+                const scenario = lines[index];
+                if (scenario && scenario.indexOf('Scenario:') >= 0) {
+                    return scenario;
+                }
+
+                return findScenario(index - 1)
+            }
 
 
-    function retriveStepAndSchenario(currentIndex) {
-        const toCheckLineIndex = currentIndex - 1;
-        const step = findStep(toCheckLineIndex);
-        const failureFirstLine = lines[currentIndex + 1]
-        const scenario = findScenario(toCheckLineIndex - 1)
-
-        return {
-            failureFirstLine,
-            index: toCheckLineIndex,
-            step,
-            scenario
+            function findStep(index) {
+                const step = lines[index];
+                const isStep = STEP_PREFIX.some(p => {
+                    return step && step.indexOf(p) >= 0;
+                })
+                if (isStep) {
+                    return step;
+                }
+                return findScenario(index - 1)
+            }
         }
-    }
-
-    function findScenario(index) {
-        const scenario = lines[index];
-        if (scenario.indexOf('Scenario:') >= 0) {
-            return scenario;
-        }
-
-        return findScenario(index - 1)
-    }
 
 
-
-    function findStep(index) {
-        const step = lines[index];
-        const isStep = STEP_PREFIX.some(p => {
-            return step.indexOf(p) >= 0;
-        })
-        if (isStep) {
-            return step;
-        }
-        return findScenario(index - 1)
-    }
-
-})();
+    })();
