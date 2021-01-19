8b04091629ac5494451bd6b0f9d23cdf9c61aff2
pillsilly <marcfrances@outlook.com>
2021-01-19 11:34:45 +0800

    Update Gitlab_helper.js

--- Gitlab_helper.js
+++ Gitlab_helper.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://gerrit.ext.net.nokia.com/*
+// @match        https://baltig.nsn-net.net/admin-ftw15/admin/*
 // @grant        none
 // ==/UserScript==
 
