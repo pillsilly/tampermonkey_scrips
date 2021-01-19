e1e59653e8ced0a39f9c6bde2f1d762731fbae22
pillsilly <marcfrances@outlook.com>
2021-01-19 11:37:36 +0800

    Update Gerrit_WebEM.js

--- Gerrit_WebEM.js
+++ Gerrit_WebEM.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gerrit
 // @namespace    http://tampermonkey.net/
-// @version      0.101
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://gerrit.ext.net.nokia.com/gerrit/c/MN/MANO/OAMCU/WEBEM/webem/*
