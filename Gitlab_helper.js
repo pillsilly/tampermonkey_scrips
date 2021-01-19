df6606a50a5571ebd41d970105bd0342fa81c286
pillsilly <marcfrances@outlook.com>
2021-01-19 11:35:23 +0800

    Update Gitlab_helper.js

--- Gitlab_helper.js
+++ Gitlab_helper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoRetry
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://baltig.nsn-net.net/admin-ftw15/admin/*
